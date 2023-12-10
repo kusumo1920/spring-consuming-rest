7a2794005d3062f88518004789f8954cde8f5951
Kesit Budi Kusumo <kesit@kernel.id>
2023-12-10 22:21:26 +0700

    initial commit

---
+++ src/main/java/id/kernel/bootcamp/springconsumingrest/SpringConsumingRestApplication.java
@@ -0,0 +1,13 @@
+package id.kernel.bootcamp.springconsumingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsumingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConsumingRestApplication.class, args);
+    }
+
+}
